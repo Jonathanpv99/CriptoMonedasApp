c15dadd63141ed91ea23e2effb91bb8953fe70a0
Jonathanpv99 <jonyvegapjv@gmail.com>
2024-08-08 11:39:24 -0600

    feat: get info fromAPI && fill the Pickers

--- components/Formulario.tsx
+++ components/Formulario.tsx
@@ -1,21 +1,36 @@
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   StyleSheet,
   Text,
   View,
 } from 'react-native';
+import axios from 'axios';
 import { Picker } from '@react-native-picker/picker';
 
 function Formulario(): React.JSX.Element {
 
   const [moneda, setMoneda] = useState(' ');
-  const [criptoMoneda, setCriptoMoneda] = useState('');
+  const [criptoMoneda, setCriptoMoneda] = useState(' ');
+  const [criptoMonedaData, setCriptoMonedaData] = useState([]);
+
+  useEffect( () => {
+    const consultarAPI = async () => {
+      const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+      const resp = await axios.get(url);
+      setCriptoMonedaData(resp.data.Data);
+    }
+
+    consultarAPI();
+  },[]);
 
   const handleGetMoneda = (moneda) => {
-    console.log('moneda', moneda);
     setMoneda(moneda);
   }
+  const handleGetCriptoMoneda = (cripto) => {
+    setCriptoMoneda(cripto);
+  }
+
   return (
     <View>
       <Text style={styles.label}>Monedas</Text>
@@ -38,12 +53,15 @@ function Formulario(): React.JSX.Element {
       <Picker
          //dropdownIconColor='black'
          dropdownIconRippleColor='gray'
-         selectedValue={moneda}
+         selectedValue={criptoMoneda}
          onValueChange={ (valor) => {
-             handleGetMoneda(valor)
+             handleGetCriptoMoneda(valor)
          }}
       >
-        <Picker.Item label='-- Seleccione --' value=""/>
+        <Picker.Item label='-- Seleccione --' value=" "/>
+        { criptoMonedaData.map( (cripto) => (
+            <Picker.Item key={cripto?.CoinInfo?.id} label={cripto?.CoinInfo?.FullName} value={cripto?.CoinInfo?.Name}/>
+          ))}
       </Picker>
     </View>
   );
